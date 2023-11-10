70697dba87ff4178cc73de7dfffb34e2acacc7d3
edel-blk <stanminesilver@gmail.com>
2023-11-10 16:46:45 -0700

    Fix create Root

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const container = document.getElementById('app')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />)
